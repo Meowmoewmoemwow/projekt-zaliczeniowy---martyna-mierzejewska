d6af11599957ec1301d09b739bfa1ab2ed13707f
Meowmoewmoemwow <jacob.rubish@gmail.com>
2026-01-28 21:54:21 +0100

    Add files via upload

---
+++ extra long factorials.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    res = 1
+    while (n != 0):
+        res *= n
+        n -= 1
+    return res
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    print(extraLongFactorials(n))
